c199a73fa1411c0ea69d00b496f055e1b764b679
Shishir Suman <shishir.suman@tarento.com>
2023-02-17 10:24:22 +0530

    code commented for you tube license

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/cloudstore/StorageService.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/cloudstore/StorageService.scala
@@ -7,7 +7,7 @@ import org.apache.tika.Tika
 import org.apache.tika.metadata.HttpHeaders
 import org.apache.tika.mime.MimeTypes
 import org.sunbird.cloud.storage.BaseStorageService
-import org.sunbird.cloud.storage.factory.{GcloudConfig, StorageConfig, StorageServiceFactory}
+import org.sunbird.cloud.storage.factory.{StorageConfig, StorageServiceFactory}
 import org.sunbird.common.exception.ServerException
 import org.sunbird.common.{Platform, Slug}
 
@@ -41,8 +41,7 @@ class StorageService {
                 val privateKeyPkcs8 = Platform.config.getString("gcloud_private_secret")
                 val privateKeyIds = Platform.config.getString("gcloud_private_bucket_project_key_id")
                 val projectId = Platform.config.getString("gcloud_private_bucket_projectId")
-                storageService = StorageServiceFactory.getStorageService(StorageConfig(storageType, storageKey, storageSecret, Option.empty,
-                  Option.apply(GcloudConfig(clientId, clientEmail, privateKeyPkcs8, privateKeyIds, projectId))))
+                storageService = StorageServiceFactory.getStorageService(StorageConfig(storageType, storageKey, storageSecret))
             }
 //            else if (StringUtils.equalsIgnoreCase(storageType, "cephs3")) {
 //                val storageKey = Platform.config.getString("cephs3_storage_key")
@@ -99,42 +98,17 @@ class StorageService {
 
     def getSignedURL(key: String, ttl: Option[Int], permission: Option[String]): String = {
       storageType match {
-        //case "gcloud" => getGCPSignedURL(key, ttl.get)
-        case "gcloud" => getService.getPutSignedURL(getContainerName, key, ttl, permission, Option.apply(getMimeType(key)))
+        case "gcloud" => getGCPSignedURL(key, ttl, permission)
+        //case "gcloud" => getService.getPutSignedURL(getContainerName, key, ttl, permission, Option.apply(getMimeType(key)))
         case _ => getService.getSignedURL (getContainerName, key, ttl, permission)
       }
     }
 
-  def getGCPSignedURL(objectName: String, ttl: Long):  String = {
-    val clientId = Platform.config.getString("gcloud_private_bucket_project_client_id")
-    val clientEmail = Platform.config.getString("gcloud_client_key")
-    val privateKeyPkcs8 = Platform.config.getString("gcloud_private_secret")
-    val privateKeyIds = Platform.config.getString("gcloud_private_bucket_project_key_id")
-    val projectId = Platform.config.getString("gcloud_private_bucket_projectId")
-    println("uploading file to clientId : ", clientId)
-    println("uploading file to clientEmail : ", clientEmail)
-    println("uploading file to privateKeyPkcs8 : ", privateKeyPkcs8)
-    println("uploading file to privateKeyIds : ", privateKeyIds)
-    println("uploading file to projectId : ", projectId)
-    println("uploading file to container : ", getContainerName)
-    println("file name getting uploaded : ", objectName)
-    val credentials = ServiceAccountCredentials.fromPkcs8(clientId, clientEmail, privateKeyPkcs8, privateKeyIds, new java.util.ArrayList[String]())
-    println("credentials created")
-    val storage = StorageOptions.newBuilder.setProjectId(projectId).setCredentials(credentials).build.getService
-    println("storage object created")
-    val extensionHeaders = new java.util.HashMap().asInstanceOf[java.util.Map[String, String]]
-    extensionHeaders.putAll(Map(HttpHeaders.CONTENT_TYPE -> MimeTypes.OCTET_STREAM).asJava)
-    //extensionHeaders.putAll(Map("x-goog-resumable" -> "start").asJava)
-    println("*** --- Changed code --- *******")
-    val blobInfo = BlobInfo.newBuilder(BlobId.of(getContainerName, objectName)).build
-    println("blob object created")
-    val expiryTime = if(ttl > 7) 7 else ttl
-    println("expiry time : ", expiryTime)
-    val url = storage.signUrl(blobInfo, expiryTime, TimeUnit.DAYS, Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
-      Storage.SignUrlOption.withExtHeaders(extensionHeaders),
-      Storage.SignUrlOption.withV4Signature);
-    println("************* url created: ", url)
-    url.toString;
+  def getGCPSignedURL(key: String, ttl: Option[Int], permission: Option[String]):  String = {
+    val additionalParams: Map[String, String] = Map("cloud_storage_client_id" -> (if (Platform.config.hasPath("cloud_storage_client_id")) Platform.config.getString("cloud_storage_client_id") else ""),
+      "cloud_storage_private_key_id" -> (if (Platform.config.hasPath("cloud_storage_private_key_id")) Platform.config.getString("cloud_storage_private_key_id") else ""),
+      "cloud_storage_project_id" -> (if (Platform.config.hasPath("cloud_storage_project_id")) Platform.config.getString("cloud_storage_project_id") else ""))
+    getService.getPutSignedURL(getContainerName, key, ttl, permission, additionalParams = Option.apply(additionalParams))
   }
 
     def getUri(key: String): String = {
@@ -153,7 +127,7 @@ class StorageService {
   }
 
   def main(args: Array[String]): Unit = {
-    val storageConfig = StorageServiceFactory.getStorageService(StorageConfig("google", "jenkins@upsmf-368011.iam.gserviceaccount.com",
+    /*val storageConfig = StorageServiceFactory.getStorageService(StorageConfig("google", "jenkins@upsmf-368011.iam.gserviceaccount.com",
       "-----BEGINPRIVATEKEY-----MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCqtqMByEGjddwE0oIkQRT4KukhPn65ozDQUfgop55VblUWeJEqmeGfTXdOTTVHpwwuYR9esrMgR5WN8IUGSLRmap9iyb4QBUV/gCjJIpsVu6HFMadBQCFceqzTqPMK6g6dwObNtDMxH6yPV47L/McwiPNoug2W+zBiRQ6YZ1GvQVY5s0KTX6EgkN/u3DW6kUu6NqcgqGuCWqUoEjss4HaX4D7DSbmKgOts/rKjwtDv9fgKLgbMlufwpxwWe/jygVUNvZumBARNIuVe+RbO6OvHb26H18KgkdDzB1VkzKX+750iIIa/KGrZHJStiw0zfri0/H0KdzbClvoOT6cBN/zVAgMBAAECggEAPN9dJsCKr0fSiaGqqFTQrmFWufT36SgIuu/PpmeGrP3nt1iMov02F8DevvwG+KLXVB5rui/MccE00eUm+gOo2DBC304CyOGoU3uWj4oYdNpwJ8m50ymT+87+m4bOC2FEjvn/EvCjpGuf84kMMS7UtDjRWtGlEOZG7XOkbbHBzdTQGldzEgsqi2T8O5I31xZ1b2LJzAVODrv7TiVElhGcUB/1MkProjhkcyJx3B3cpClwY8Lq2R2urTf4NxMnmh/PmUfBzLQLauSDI/MH9NN57J1M/5uWYAIY/eaf8BtqEsbrXLmBP1WfNchXbfXLeadaiAX45ukt0y103qd0TmJa7QKBgQDdvgTcjKMddzfU6PeBXO3upl2FVLA6B89J/QiEKoeg4bkM2C3pNkGvgVA3HfHfauMhhwFee8mP14HLZQvb+0k9tL64CiznkuEfOBApkXJDsW0iAN3TwMj5zVRAVHWBRcexMt74CdySuKDOkV9G5feOXfdhOZM6z8LSfGs+2lYbQwKBgQDFFmj8Mtv4Pv5zsF1/UeFnuijkHO84dNYnflTB5Pmwb4Z5rhnJzG446cxr9f7E/+3yjd+xtBQf5ttPwvCBbZR20RO2jA5o/qijXaYHCjlE7yOpAfgU+p5K3JH9bTMLuPsSVaxBof7cFoqjFalVGmpR1qAj4UGHc9mTnV6CGCbqBwKBgQCTI+RV9XzHsLR7s5uJXAEGu56TOv81grkqf52HFjGpsB77RvgwKLCtpUF1UYmOl37gYJWn/Lxjlr2qGgMkljqjl6x2s0nY4L5B2RHgg4MvKC0iwzBvsx2ppXaiuWi/v24jR35tWR3kvl72s8Bla3Q6JGBjQ7FO9U5yHd2Md5VrwQKBgAzyQOk4KgzvjmVpE2s2pLjcmK0LXYd23U5w1P57nQ9C9DFwB0+jNyZT7VK2MQsdyLKjMSuKKbxCvOtLYeMOoK8BYusd3iB1gfxhPXO+7y4hC1WhxHsUT2uZe5mLH8xIVW3J5OvWyVgJvwehd6MYfh1sHM7ekCBmsscokjm3fm7nAoGBAL5PXhD6rCaHGOo0KXEA0S6rzMI6qBzQvMyOVj7b0lwey6q+G2xl7Cc9IUmxVzhBe7daD6QSQ4dU91ZKaIysopfZWibHFcQm6I6FJI3ha73EOB2zyyl3xlBxK9fMQVN8gELdXhA8DBuMD+Qxj6NrbqteFJLCyz7ATtETSb3+hP+G-----ENDPRIVATEKEY-----",
       None, Option.apply(GcloudConfig("113740098487205958998",
         "jenkins@upsmf-368011.iam.gserviceaccount.com",
@@ -161,7 +135,7 @@ class StorageService {
         "6aef3a75efe29225e6347244de3e8f1ddd8437df","upsmf-368011"))))
     println("storage config : "+ storageConfig)
     val str = storageConfig.getPutSignedURL("dev-public-upsmf", "sample.txt", Option.apply(7), None, Option.apply("application/octet-stream"))
-    println(str)
+    println(str)*/
 
   }
 
