1fd7d76906b44c6cdceab9bfd2b81c655a91f3c9
Shishir Suman <shishir.suman@tarento.com>
2023-04-26 19:13:30 +0530

    removing commented code

--- assessment-api/assessment-service/app/handlers/CompetencyExcelParser.scala
+++ assessment-api/assessment-service/app/handlers/CompetencyExcelParser.scala
@@ -44,38 +44,10 @@ object CompetencyExcelParser {
     listData.add(competencyId)
     listData.add(competency)
     listData.add(competencyLevelId)
-    //val competencyLevel = rowContent.apply(2).trim
     data.put(competencyMapping, listData)
     data
   }
 
-
-  /*def getCompetenciesData1(sheet: XSSFSheet): List[util.Map[String, AnyRef]] = {
-    val column = sheet.asScala.drop(1).map(row =>
-      if (sheet.getWorkbook.getSheetIndex(sheet) == 1 || sheet.getWorkbook.getSheetIndex(sheet) == 8)
-        row.getCell(4)
-      else
-        row.getCell(5)
-    )
-
-    val formatter = new DataFormatter()
-    val uniqueCompetencies = column.map(cell => formatter.formatCellValue(cell)).toList
-
-    val rows = sheet.asScala.drop(1)
-    getData = rows.flatMap(row => {
-      val cell = if (sheet.getWorkbook.getSheetIndex(sheet) == 1 || sheet.getWorkbook.getSheetIndex(sheet) == 8)
-        row.getCell(4)
-      else
-        row.getCell(5)
-      if (cell != null && uniqueCompetencies.contains(cell.getStringCellValue)) {
-        //if(row.getCell(1)!=null||row.getCell(2)!=null||row.getCell(2)!=null||row.getCell(4)!=null||row.getCell(5)!=null)
-        parseCompetencyData(sheet.getRow(rowNum))
-      } else {
-        None
-      }
-    }).toList
-    getData
-  }*/
    def getCompetency(file: File): List[java.util.Map[String, AnyRef]] = {
       logger.info("enter into the getCompetency method")
       val finalData: mutable.Map[String, List[Map[String, AnyRef]]] = mutable.Map.empty
@@ -83,11 +55,7 @@ object CompetencyExcelParser {
         val workbook = new XSSFWorkbook(new FileInputStream(file))
         (0 until workbook.getNumberOfSheets)
           .foreach(index => {
-           // if (index ==1) {
             getData = getCompetenciesData(workbook.getSheetAt(index))
-           /*}else {
-            getData=getCompetenciesDataFromSheet(workbook.getSheetAt(index))
-          }*/
             val convertedData = getData.map(_.asScala.toMap)
             finalData += (workbook.getSheetName(index) -> convertedData)
             getData = finalData.toList.flatMap { case (_, maps) => maps.map(convertMap) }
@@ -99,23 +67,6 @@ object CompetencyExcelParser {
       }
     }
 
-  /*def getCompetenciesData2(sheet: XSSFSheet): List[util.Map[String, AnyRef]] = {
-    val columnIdx = if (sheet.getWorkbook.getSheetIndex(sheet) == 1 || sheet.getWorkbook.getSheetIndex(sheet) == 8) 4 else 5
-    val rows = sheet.asScala.drop(1)
-    val formatter = new DataFormatter()
-
-    def getCell(row: Row, colIdx: Int): Option[Cell] = {
-      val cell = row.getCell(colIdx)
-      Option(cell).filter(c => Option(formatter.formatCellValue(c)).exists(_.nonEmpty))
-    }
-
-    val uniqueCompetencies = rows.flatMap(row => getCell(row, columnIdx)).map(cell => formatter.formatCellValue(cell)).toList
-
-    rows.flatMap(row => {
-      getCell(row, columnIdx).filter(cell => uniqueCompetencies.contains(formatter.formatCellValue(cell))).map(row => parseCompetencyData(sheet.getRow(rowNum))).flatten
-    }).toList
-  }*/
-
   def convertMap(map: Map[String, AnyRef]): util.Map[String, AnyRef] = {
     val javaMap = new util.HashMap[String, AnyRef]()
     map.foreach { case (k, v) => javaMap.put(k, v) }
@@ -123,16 +74,6 @@ object CompetencyExcelParser {
   }
 
   def getCompetenciesData(sheet: XSSFSheet): List[util.Map[String, AnyRef]] = {
-    /* val column = sheet.asScala.drop(1).map(row =>
-       if (sheet.getWorkbook.getSheetIndex(sheet) == 1 || sheet.getWorkbook.getSheetIndex(sheet) == 8)
-         row.getCell(4)
-       else
-         row.getCell(5)
-     ).toList*/
-
-    /*val formatter = new DataFormatter()
-    val uniqueCompetencies = column.map(cell => formatter.formatCellValue(cell)).toList*/
-
     val rows = sheet.asScala.drop(1)
     getData = rows.flatMap(row => {
       val rowValue = {
