528ec8a1282adaf78d8d6b7ae8a2f21cdd548911
anil <anilkumar.kammalapalli@tarento.com>
2023-05-31 06:42:11 +0530

    removed unwanted code

--- assessment-api/qs-hierarchy-manager/src/main/scala/org/sunbird/utils/CryptoUtil.scala
+++ assessment-api/qs-hierarchy-manager/src/main/scala/org/sunbird/utils/CryptoUtil.scala
@@ -7,17 +7,6 @@ import javax.crypto.Mac
 import scala.util.{Try, Either, Left, Right}
 object CryptoUtil {
   private val US_ASCII = StandardCharsets.US_ASCII
-
-  //  def generateHMAC(payLoad: String, secretKey: String, algorithm: String): Array[Byte] = {
-  //    try {
-  //      val mac = Mac.getInstance(algorithm)
-  //      mac.init(new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), algorithm))
-  //      mac.doFinal(payLoad.getBytes(US_ASCII))
-  //    } catch {
-  //      case _: NoSuchAlgorithmException | _: InvalidKeyException => null
-  //    }
-  //  }
-
   def generateRSASign(payLoad: String, key: PrivateKey, algorithm: String): Array[Byte] = {
     val sign: Signature = Signature.getInstance(algorithm)
     sign.initSign(key)
